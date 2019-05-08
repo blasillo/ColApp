67ec4ebc748b2bfcec54f27a003af210159a301a
Blas Torregrosa Garcia <torgarbl@jcyl.es>
2019-05-08 14:55:46 +0200

    Inicial

---
+++ src/main/java/es/jcyl/colapp/ColappApplication.java
@@ -0,0 +1,13 @@
+package es.jcyl.colapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColappApplication.class, args);
+	}
+
+}
