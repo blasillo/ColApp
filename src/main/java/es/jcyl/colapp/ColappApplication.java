d22d188b981504981754c668a834cc84d5b39f47
Blas Torregrosa Garcia <torgarbl@jcyl.es>
2019-05-09 14:13:39 +0200

    Inicial

--- src/main/java/es/jcyl/colapp/ColappApplication.java
+++ src/main/java/es/jcyl/colapp/ColappApplication.java
@@ -3,7 +3,10 @@ package es.jcyl.colapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 import es.jcyl.colapp.config.SeguridadConfig;
 import es.jcyl.colapp.controladores.PrincipalCtrl;
@@ -15,5 +18,5 @@ public class ColappApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ColappApplication.class, args);
 	}
-
+	
 }
